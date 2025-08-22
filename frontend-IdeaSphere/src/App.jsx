4c76ad7715c05dc3aa49481f00263fc0add7005b
safeer <safeeralam997@gmail.com>
2025-08-22 20:27:27 +0530

    initial commit

---
+++ frontend-IdeaSphere/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome To IdeaSphere !</h1>
+      <p>Your ideas, your space.</p>
+    </div>
+  );
+}
+
+export default App;
