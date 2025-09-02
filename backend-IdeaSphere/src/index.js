3c38219ef71ee47d8af9b6e45fbd9a4cecb7ee99
safeer <safeeralam997@gmail.com>
2025-09-02 19:55:34 +0530

    fix server running even if db conn failed

--- backend-IdeaSphere/src/index.js
+++ backend-IdeaSphere/src/index.js
@@ -1,19 +1,19 @@
 import express from 'express';
-import dotenv from "dotenv";
-import {connectDb} from "./config/db.js"
+import dotenv from 'dotenv';
+import { connectDb } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json()); 
-
-
-connectDb();
-
+app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
-
+try {  
+  await connectDb();
+  app.listen(PORT, () => {
+    console.log(`Server running on port:${PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
